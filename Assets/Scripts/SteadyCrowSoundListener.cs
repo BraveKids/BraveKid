a1fac645fa911a29c0b1cab41d3febf426e0dc07
Tommaso Celata <t.celata@gmail.com>
2016-01-31 14:00:13 +0100

    SoundManager

---
+++ Assets/Scripts/SteadyCrowSoundListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteadyCrowSoundListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            if (!other.GetComponent<CharacterControllerScript>().isSlow()) {
+                other.GetComponent<CharacterControllerScript>().canMove(false);
+                other.GetComponent<CharacterControllerScript>().GameOver();
+                gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                gameObject.GetComponent<Animator>().SetBool("isSpotted", true);
+            }
+        }
+    }
+}
