61e06283abcd62d6c757d9fefff3d62e0ed92685
andrea29292 <andrea.ceccarelli29292@gmail.com>
2016-01-31 12:45:47 +0100

    new menu and sound manager

---
+++ Assets/Scripts/Misc/SoundManager.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+	
+	public static SoundManager instance = null;
+	Dictionary<string,AudioClip> allMusics;
+	Dictionary<string, AudioClip> allEffect;
+	
+	// source
+	AudioSource musicSource = null;
+	
+	// clips 
+	public AudioClip track1;
+	public AudioClip track2;
+	public AudioClip track3_1;
+	public AudioClip track3_2;
+	public AudioClip track3_3;
+
+
+	//audio effects
+
+	public AudioClip button;
+	public AudioClip laugh;
+	public AudioClip swing;
+	public AudioClip goodCat;
+	public AudioClip badCat;
+	public AudioClip goodBird;
+	public AudioClip badBird;
+	public AudioClip rock;
+	public AudioClip lizard;
+	public AudioClip mouseTrap;
+	public AudioClip goodMouse;
+	public AudioClip badMouse;
+	public AudioClip light;
+	public AudioClip hey;
+	public AudioClip wind;
+	public AudioClip crows;
+	public AudioClip chair;
+	public AudioClip sleepyGranny;
+	public AudioClip scaryGranny;
+	public AudioClip sleepyDog;
+	public AudioClip barfDog;
+
+
+	int level = -1;	//default value
+	
+	string musicPlayed;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// singleton
+		if (instance == null) {
+			instance = this;
+			//DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+		
+		allMusics = new Dictionary<string, AudioClip> ();
+
+
+		allEffect = new Dictionary<string, AudioClip > ();
+		allEffect.Add ("button", button);
+		allEffect.Add ("laugh",laugh);
+		allEffect.Add ("swing",swing);
+		allEffect.Add ("goodCat", goodCat);
+		allEffect.Add ("badCat", badCat);
+		allEffect.Add("goodBird", goodBird);
+		allEffect.Add("badBird", badBird);
+		allEffect.Add("rock", rock);
+		allEffect.Add("lizard", lizard);
+		allEffect.Add("mouseTrap", mouseTrap);
+		allEffect.Add("goodMouse", goodMouse);
+		allEffect.Add("badMouse", badMouse);
+		allEffect.Add("light", light);
+		allEffect.Add("hey", hey);
+		allEffect.Add("wind", wind);
+		allEffect.Add("crows", crows);
+		allEffect.Add("chair", chair);
+		allEffect.Add("sleepyGranny", sleepyGranny);
+		allEffect.Add("scaryGranny", scaryGranny);
+		allEffect.Add("sleepyDog", sleepyDog);
+		allEffect.Add("barfDog", barfDog);
+		
+		musicSource = GetComponent<AudioSource> () as AudioSource;
+		musicSource.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (level != SaveLoad.savedGame.level) {
+
+
+			switch (SaveLoad.savedGame.level) {
+			case -1:
+				SetBackgroundMusic ("lullaby");
+				break;
+			case 0:
+				SetBackgroundMusic ("tutorial");
+				break;
+			case 1:
+				SetBackgroundMusic ("school");
+				break;
+				
+			case 2:
+				SetBackgroundMusic ("gorilla");
+				break;
+				
+			case 5:
+				SetBackgroundMusic ("knight");
+				break;
+			default:
+				SetBackgroundMusic ("lullaby");
+				break;
+				
+			}
+			
+			
+		}
+	}
+	
+	public void SetVolume (float _volume)
+	{
+		musicSource.volume = _volume;
+	}
+	
+	public void SetBackgroundMusic (string background)
+	{
+		if (allMusics.ContainsKey (background)) {
+			musicSource.clip = allMusics [background];
+			level = SaveLoad.savedGame.level;
+			musicSource.Play ();
+		}
+		
+		
+		
+	}
+	
+	public void SetMusic (bool _music)
+	{
+		if (musicSource.isPlaying == true && _music == false) {
+			musicSource.Pause ();
+		}
+		if (musicSource.isPlaying == false && _music == true) {
+			musicSource.Play ();
+		}
+	}
+
+	public void playAudioEffect (string audioEffect)
+	{
+		Debug.Log(allEffect["button"].ToString());
+		AudioSource audio = gameObject.AddComponent<AudioSource>();
+		audio.clip = allEffect ["button"];
+		audio.Play ();
+	}
+
+	public AudioSource playLoopEffect (string audioEffect, bool loop)
+	{
+		AudioSource audio = new AudioSource ();
+		audio.loop = loop;
+		audio.clip = allEffect [audioEffect];
+		audio.Play ();
+		return audio;
+	}
+	
+}
--- Assets/Scripts/UI/menuManager.cs
+++ Assets/Scripts/UI/menuManager.cs
@@ -3,12 +3,16 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.IO;
 
 public class menuManager : MonoBehaviour
 {
 
 	//public List<Button> buttons;
 	public List<List<GameObject>> buttons;
+	public List<List<Sprite>> coloredSprites;
+	public List<List<Sprite>> graySprites;
+	
 	// Use this for initialization
 	int row;
 	int column;
@@ -19,14 +23,25 @@ public class menuManager : MonoBehaviour
 	float scaleX;
 	float scaleY;
 	float scaleZ;
-	bool canContinue = true;
+	bool canContinue = false;
 	GameObject newGame;
 	GameObject continueGame;
 	GameObject credits;
 	GameObject exitGame;
+	Color notSelectedColor = new Color (200f, 200f, 200f);
+	public Sprite coloredNewGame;
+	public Sprite coloredContinue;
+	public Sprite coloredCredits;
+	public Sprite coloredExit;
+	public Sprite grayNewGame;
+	public Sprite grayContinue;
+	public Sprite grayCredits;
+	public Sprite grayExit;
+	public AudioClip button;
 
 	void Start ()
 	{
+		
 		GameObject newGame = transform.FindChild ("newGame").gameObject;
 		GameObject continueGame = transform.FindChild ("continue").gameObject;
 		GameObject credits = transform.FindChild ("credits").gameObject;
@@ -34,8 +49,18 @@ public class menuManager : MonoBehaviour
 
 		List<GameObject> colonna1 = new List<GameObject> (){newGame, credits};
 		List<GameObject> colonna2 = new List<GameObject> (){continueGame,exitGame};
-
 		buttons = new List<List<GameObject>> (){colonna1,colonna2};
+		
+		List<Sprite> colored1 = new List<Sprite> (){coloredNewGame, coloredContinue};
+		List<Sprite> colored2 = new List<Sprite> (){coloredCredits, coloredExit};
+		coloredSprites = new List<List<Sprite>> (){colored1,colored2};
+
+		List<Sprite> gray1 = new List<Sprite> (){grayNewGame, grayContinue};
+		List<Sprite> gray2 = new List<Sprite> (){grayCredits, grayExit};
+		graySprites = new List<List<Sprite>> (){gray1,gray2};
+		
+
+
 		row = 0;
 		column = 0;
 		t = 0;
@@ -45,9 +70,19 @@ public class menuManager : MonoBehaviour
 		scaleY = buttons [0] [0].transform.localScale.y;
 		scaleZ = buttons [0] [0].transform.localScale.z;
 
+		buttonState (0, 0, true);
+		
+		buttonState (0, 1, false);
+		
+		buttonState (1, 1, false);
+
+		if (File.Exists (SaveLoad.SAVING_PATH)) {
+			canContinue = true;
+		}
 		if (!canContinue) {
 			disableContinue ();
-
+		} else {
+			buttonState (1, 0, false);			
 		}
 	}
 	
@@ -55,7 +90,6 @@ public class menuManager : MonoBehaviour
 	void Update ()
 	{
 		selected = buttons [column] [row].transform.name;
-		Debug.Log(selected);
 		if (Input.GetKeyDown (KeyCode.Return)) {
 			if (selected == "continue") {
 				SaveLoad.Load ();
@@ -66,7 +100,7 @@ public class menuManager : MonoBehaviour
 				Application.LoadLevel ("Gameplay");
 			}
 
-			if(selected == "exitGame"){
+			if (selected == "exitGame") {
 				Application.Quit ();
 			}
 		}
@@ -108,14 +142,22 @@ public class menuManager : MonoBehaviour
 
 	void changeButton (int column, int row)
 	{
+		if (this.column == column && this.row == row) {
+			return;
+		}
 		if (buttons [column] [row].transform.name == "continue" && !canContinue) {
 			return;
 		} 
-		buttons [this.column] [this.row].transform.GetChild (0).transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
 
+		buttonState (this.column, this.row, false);
 		this.column = column;
 		this.row = row;
-			
+		buttonState (this.column, this.row, true);
+
+		AudioSource audio = gameObject.AddComponent<AudioSource>();
+		audio.clip = button;
+		audio.Play ();
+
 			
 		
 	}
@@ -134,7 +176,28 @@ public class menuManager : MonoBehaviour
 
 	void disableContinue ()
 	{
-		continueGame.transform.GetComponentInChildren<Text> ().color = Color.gray;
-	
+		buttons [1] [0].GetComponentInChildren<Text> ().color = Color.gray;
+		buttons [1] [0].GetComponent<Image> ().color = Color.black;
+		
+
+	}
+
+	void buttonState (int column, int row, bool isSelected)
+	{
+		if (!isSelected) {
+			buttons [column] [row].GetComponent<Image> ().sprite = graySprites [column] [row];
+			buttons [column] [row].transform.GetChild (0).transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
+			
+		}
+
+		if (isSelected) {
+			buttons [column] [row].GetComponent<Image> ().sprite = coloredSprites [column] [row];
+			
+		}
+	}
+
+	public void playAudioEffect (string audioEffect)
+	{	
+
 	}
 }
